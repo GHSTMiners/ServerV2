c39f10a3881ac789f1ea0f8491cd9ae89cc095b5
Benjamin Martens <info@1-zero.nl>
2022-02-14 21:04:14 +0100

    Buildings now work

---
+++ src/Game/Managers/PlayerWalletManager/index.ts
@@ -0,0 +1,9 @@
+import Player from "../../Objects/Player";
+
+export default class PlayerWalletManager extends Phaser.GameObjects.GameObject {
+    constructor(scene : Phaser.Scene, player : Player) {
+        super(scene, "PlayerWalletManager")
+    }
+
+    
+}

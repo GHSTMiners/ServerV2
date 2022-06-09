c337d4ced00a3d66e5744c2213a73eee50f4eb78
Gotchinomics <gotchinomics@gmail.com>
2022-06-09 21:47:13 +0200

    Updated exchange value  to support decimals

--- src/Schemas/World/ExchangeEntry.ts
+++ src/Schemas/World/ExchangeEntry.ts
@@ -2,5 +2,5 @@ import { Schema, type } from "@colyseus/schema"
 
 export class ExchangeEntry extends Schema {
     @type ("int32") crypto_id : number = 0;
-    @type ("int32") usd_value: number = 1;
+    @type ("float32") usd_value: number = 1;
 }

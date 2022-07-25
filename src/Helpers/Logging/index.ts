20b70d7e74b4187284eb510e7c3297ebee930b4d
Benjamin Martens <benjamin.martens@politie.nl>
2022-07-25 15:19:23 +0200

    fix small issue with logging

--- src/Helpers/Logging/index.ts
+++ src/Helpers/Logging/index.ts
@@ -4,12 +4,16 @@ import http from 'http';
 
 export default class Logging {
     public static submitEvent(event : string, score : number, request: http.IncomingMessage, gotchi : number | undefined, wallet : string | undefined, chainId : number | undefined = 137) {
-        axios.post(`${Config.apiURL}/logging`, { event: event, score: score, ip_address: request.headers['cf-connecting-ip'] as string || request.socket ? request.socket.remoteAddress : "0.0.0.0", gotchi: gotchi, wallet: wallet, chain_id: chainId }, {
+        try{
+        axios.post(`${Config.apiURL}/logging`, { event: event, score: score, ip_address: request.headers['cf-connecting-ip'] as string || request.socket ? request.socket.remoteAddress ? request.socket.remoteAddress : "0.0.0.0" : "0.0.0.0", gotchi: gotchi, wallet: wallet, chain_id: chainId }, {
             headers: {
                 'X-API-KEY': Config.apiKey
             }
         }).catch(exception=> {
             console.warn(exception);
         })
+        } catch (exception) {
+
+        }
     }
 }

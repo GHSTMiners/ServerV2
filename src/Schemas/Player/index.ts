aba3f0b773de871420f84bd96ed619d2a43b3082
Benjamin Martens <info@1-zero.nl>
2022-12-31 17:57:37 +0100

    Removed cargo filter

--- src/Schemas/Player/index.ts
+++ src/Schemas/Player/index.ts
@@ -23,7 +23,6 @@ export class Player extends Schema {
     @type ( [Vital] ) vitals = new ArraySchema<Vital>();
     @filter(function(this: Player, client: Client, value: Player['skills']) { return this.playerSessionID == client.sessionId})
     @type ( [Skill] ) skills = new ArraySchema<Skill>();
-    @filter(function(this: Player, client: Client, value: Player['cargo']) { return this.playerSessionID == client.sessionId})
     @type ({ map: CargoEntry }) cargo = new MapSchema<CargoEntry>();
     @type ({ map: WalletEntry }) wallet = new MapSchema<WalletEntry>();
     @filter(function(this: Player, client: Client, value: Player['explosives']) { return this.playerSessionID == client.sessionId})

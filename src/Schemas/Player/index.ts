0c8533a64e70cb4d63b096115e6c2c392a7b6a16
Benjamin Martens <info@1-zero.nl>
2023-02-17 21:10:14 +0100

    Cleaning up a bit

--- src/Schemas/Player/index.ts
+++ src/Schemas/Player/index.ts
@@ -16,18 +16,18 @@ export class Player extends Schema {
     @type ("number") gotchiID: number = 0;
     @type ("string") walletAddress: string = "";
     @type ("string") chatColor: string = "#ffffff";
-    @filter(function(this: Player, client: Client, value: Player['peerID']) { return this.playerSessionID == client.sessionId})
+    @filter(function(this: Player, client: Client, value: Player['peerID']) { return this.playerSessionID == client.sessionId })
     @type ("string") peerID : string = ""
     @type (PlayerState) playerState : PlayerState = new PlayerState();
     @type ({ map: Upgrade}) upgrades = new MapSchema<Upgrade>();
-    @filter(function(this: Player, client: Client, value: Player['vitals']) { return this.playerSessionID == client.sessionId})
+    @filter(function(this: Player, client: Client, value: Player['vitals']) { return this.playerSessionID == client.sessionId })
     @type ( [Vital] ) vitals = new ArraySchema<Vital>();
-    @filter(function(this: Player, client: Client, value: Player['skills']) { return this.playerSessionID == client.sessionId})
+    @filter(function(this: Player, client: Client, value: Player['skills']) { return this.playerSessionID == client.sessionId })
     @type ( [Skill] ) skills = new ArraySchema<Skill>();
     @type ({ map: CargoEntry }) cargo = new MapSchema<CargoEntry>();
     @type ({ map: WalletEntry }) wallet = new MapSchema<WalletEntry>();
     @type ({ map: ConsumableEntry }) consumable = new MapSchema<ConsumableEntry>();
-    @filter(function(this: Player, client: Client, value: Player['explosives']) { return this.playerSessionID == client.sessionId})
+    @filter(function(this: Player, client: Client, value: Player['explosives']) { return this.playerSessionID == client.sessionId })
     @type ({ map: ExplosiveEntry}) explosives = new MapSchema<ExplosiveEntry>();
     @type ({ map: StatisticEntry}) statistics = new MapSchema<StatisticEntry>();
 }

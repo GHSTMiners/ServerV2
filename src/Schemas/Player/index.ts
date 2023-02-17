dd8c62b396812e7156b1b3b52e09157decb7bd51
Benjamin Martens <info@1-zero.nl>
2023-02-17 21:01:50 +0100

    Added consumable to schema

--- src/Schemas/Player/index.ts
+++ src/Schemas/Player/index.ts
@@ -1,6 +1,7 @@
 import { Schema, MapSchema, ArraySchema, type, filter } from "@colyseus/schema"
 import { PlayerState } from "./PlayerState";
 import { WalletEntry } from "./WalletEntry";
+import { ConsumableEntry } from "./ConsumableEntry";
 import { CargoEntry } from "./CargoEntry";
 import { Vital } from "./Vital";
 import { Skill } from "./Skill";
@@ -25,6 +26,7 @@ export class Player extends Schema {
     @type ( [Skill] ) skills = new ArraySchema<Skill>();
     @type ({ map: CargoEntry }) cargo = new MapSchema<CargoEntry>();
     @type ({ map: WalletEntry }) wallet = new MapSchema<WalletEntry>();
+    @type ({ map: ConsumableEntry }) consumable = new MapSchema<ConsumableEntry>();
     @filter(function(this: Player, client: Client, value: Player['explosives']) { return this.playerSessionID == client.sessionId})
     @type ({ map: ExplosiveEntry}) explosives = new MapSchema<ExplosiveEntry>();
     @type ({ map: StatisticEntry}) statistics = new MapSchema<StatisticEntry>();
